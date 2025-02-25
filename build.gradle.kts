a9c161ebea9fd3c8a4caeff8cb445ffe214abd57
Gani Karaoğlu <ganikaraoglu@msn.com>
2025-02-25 17:40:41 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.20"
     `maven-publish`
 }
 
 group = "com.github.finbot.explicit"
-version = "0.0.4"
+version = "0.0.5"
 
 dependencies {
     compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.23.5")
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.7")
-    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 kotlin {
